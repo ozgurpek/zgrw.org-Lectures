643c2751cb17cd84e117c54b06314650c71f8413
Pekcagliyan, Ozgur (CT DD DS EU TR DF FA-2) <ozgur.pekcagliyan@siemens.com>
2016-10-01 13:26:08 +0200

    Java Solution for Max Contigous Sequence Sum

---
+++ Java/Max Cont. Sequence/maxseq.java
@@ -0,0 +1,23 @@
+
+public class maxseq
+{
+	public static void main(String []args)
+	{
+		int []arr = {8,-7,5,3,87,1,-156,9,0};
+		maxseq mxs = new maxseq();
+		int res = mxs.maxSeq(arr);
+		System.out.println("Max seq is: " + res);
+	}
+	
+	public int maxSeq(int []arr)
+	{
+		int sumSoFar = 0;
+		int sumMax = 0;
+		for(int i = 0; i < arr.length; ++i)
+		{
+			sumSoFar = Math.max(0, sumSoFar + arr[i]);
+			sumMax = Math.max(sumMax, sumSoFar);
+		}
+		return sumMax;
+	}
+}
